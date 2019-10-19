847b35c25cffe7006232d1d74de7732cf2fddb5c
Atomic-Waffles <56767692+Atomic-Waffles@users.noreply.github.com>
2019-10-19 15:29:04 -0400

    Add files via upload

--- src/primary_code/Charactar.java
+++ src/primary_code/Charactar.java
@@ -1,9 +1,13 @@
 package primary_code;
 
+import java.util.*;
+
 public class Charactar {
 	//char stats
-	public int health, strength, magic, defense, speed, luck, skill;
-	public Charactar(int h, int str, int m, int d, int sp, int l, int ski) {
+	public int health, strength, magic, defense, speed, luck, skill,resistance;
+	public ArrayList<Weapon> weapons;
+	public String name;
+	public Charactar(int h, int str, int m, int d, int sp, int l, int ski, int r) {
 		this.health = h;
 		this.strength = str;
 		this.magic = m;
@@ -11,6 +15,7 @@ public class Charactar {
 		this.speed = sp;
 		this.luck = l;
 		this.skill = ski;
+		this.resistance = r;
 	}
 	public Charactar() {
 		this.health = 20;
@@ -20,7 +25,7 @@ public class Charactar {
 		this.speed = 10;
 		this.luck = 10;
 		this.skill = 10;
-		
+		this.resistance = 5;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/primary_code/Weapon.java
+++ src/primary_code/Weapon.java
@@ -1,7 +1,13 @@
 package primary_code;
 
 public class Weapon {
-
+	//changes strength or magic 
+	public boolean isMagic = false;
+	public String name;
+	public int might, weight, hit;
+	public Weapon() {
+		this;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
